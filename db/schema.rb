2bf9588eb00c4c9dc8e98c73fbdc05026ff5a3ad
NemanjaMandic <nemanjans84@gmail.com>
2016-02-04 03:10:18 +0100

    scaffolded cart and cart_item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203221616) do
+ActiveRecord::Schema.define(version: 20160204020451) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "cart_items", ["cart_id"], name: "index_cart_items_on_cart_id"
+  add_index "cart_items", ["product_id"], name: "index_cart_items_on_product_id"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name",                   null: false
